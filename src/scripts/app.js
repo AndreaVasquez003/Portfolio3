445569798f16bb1c364f4a9c4c34f97b607fe74f
AndreaVasquez003 <151018092+AndreaVasquez003@users.noreply.github.com>
2025-06-04 19:40:56 +0200

    avancement"menuFinal"

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,9 @@
 "use strict"
+
+const menuBurgerBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+
+menuBurgerBtn.addEventListener('click', () => {
+	menuBurgerBtn.classList.toggle('open');
+	menu.classList.toggle('open');
+});
